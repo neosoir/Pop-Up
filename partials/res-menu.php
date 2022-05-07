ad68434dde674c1856524c8acb5f75fb31651405
Neo Soir <leonard@neosoir.com>
2022-05-08 01:43:05 +0200

    [fix] dashboard menu items

--- partials/res-menu.php
+++ partials/res-menu.php
@@ -9,11 +9,11 @@ if ( !function_exists( 'res_menu_popup' ) ) {
     add_action( 'admin_menu', 'res_menu_popup' );
 
     function res_menu_popup() {
-        
+
         add_menu_page(
             'Menú Pop-Up',
             'Menú Pop-Up',
-            'manage-options',
+            'manage_options',
             'res_popup',
             'res_options_menu_popup',
             'dashicons-megaphone',
