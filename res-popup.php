45f9aa5f0b6119eb881de20bfc4a5e06ee18561b
Neo Soir <leonard@neosoir.com>
2022-05-08 01:04:38 +0200

    [init] Res pop up plugin

---
+++ res-popup.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name:         Res pop-up
+ * Plugin URI:          https://neoslab.online
+ * Description:         plugin para pop up
+ * Version:             1.0.0
+ * Author:              Neos Lab
+ * Author URI:          https://newtheme.eu
+ * License:             GPL2
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:         pop-up
+ * Domain Path:         /languages
+ */
+
