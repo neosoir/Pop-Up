fc830ef3328c0cdb6a4a3541a894aac2b97d6838
Neo Soir <leonard@neosoir.com>
2022-05-08 01:23:01 +0200

    [add] uninstall archive

---
+++ activador.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * Here can make some thing to activate plugin (like activate plugin).
+ */
---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * When uninstall plugin
+ */
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+/**
+ * Add code to delete:
+ *      Data base.
+ *      Clean cache.
+ *      Clean permalinks.
+ *      etc.
+ */
