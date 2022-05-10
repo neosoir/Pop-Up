1e95adb5af81524fb2f3fab563ae269c8bcf0c10
Neo Soir <leonard@neosoir.com>
2022-05-10 18:48:18 +0200

    [add] res-modal-edit.php

---
+++ admin/res-modal-edit.php
@@ -0,0 +1,44 @@
+<!-- Modal urs bootstrap-->
+
+<div class="modal modalData" tabindex="-1" id="modalPreview">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="row">
+
+            <div class="col-4 col-sm-4">
+                <div class="class-imgFondo" style="background-img: url(/_curso02/wp-content/uploads/2020/07/running-573762_640.jpg)"></div>
+            </div>
+
+            <div class="col-8 col-sm-8">
+                <div class="mb-3 mt-2 wow">
+                    <div class="col-12 col-sm-12 d-flex justify-contend-end pd-btnclose">
+                        <div class="col-1">
+                            <div class="boton-close"><span aria-hidden="true"></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-4-row">
+                <div class="col-12 pd-textmodal">
+                   <div class="content-popup text-center">
+                       <h4>Título</h4>
+                       <h6>Subtítulo</h6>
+                       <p>
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae praesentium dolorum ipsam doloribus ea facere eaque at, accusantium saepe? Libero possimus aperiam excepturi, aliquid tempore labore quia neque soluta quaerat!
+                       </p>
+                   </div> 
+                </div>
+            </div>
+
+            <div class="mb-5 row">
+                <div class="col-12 pd-textmodal btn-callAction text-center">
+                    <a href="#" class="btn btn-success" target="_blank">
+                        <span>título botón</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
