2b9599e26d616911ed1cb191d876a02cb5293a53
Neo Soir <leonard@neosoir.com>
2022-07-04 01:58:55 +0200

    [fix] add bootstrap in fronten part

--- public/res-public-popup.php
+++ public/res-public-popup.php
@@ -7,27 +7,106 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_style_public');
 
 function enqueue_style_public() {
+
+    //Encolando la libreria de bootstrap 5.1
+    wp_enqueue_style(
+        'bootstrap-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap.rtl-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap.rtl.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-grid-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-grid.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-grid-rtl-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-grid.rtl.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-reboot-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-reboot-rtl-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-reboot.rtl.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-utilities-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+    wp_enqueue_style(
+        'bootstrap-utilities-rtl-css',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/css/bootstrap-utilities.rtl.min.css',
+        [],
+        '5.1.0',
+        'all'
+    );
+
     // Css
     wp_enqueue_style(
         'admin-style',
-        plugin_dir_url(__DIR__) . 'public/css/app.css',
+        plugin_dir_url(__DIR__) . 'public/css/app.min.css',
         [],
         '1.0.0',
         'all'
     );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_public');
 
 function enqueue_scripts_public() {
+
+    // Bootstrap library
+    wp_enqueue_script(
+        'bootstrap-min',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/js/bootstrap.min.js',
+        ['jquery'],
+        '5.1.0',
+        true
+    );
+
+    wp_enqueue_script(
+        'bootstrap-bundle',
+        plugin_dir_url( __DIR__ ) . 'helpers/bootstrap-5.1/js/bootstrap.bundle.min.js',
+        ['jquery'],
+        '5.1.0',
+        true
+    );
+
     // Js
     wp_enqueue_script(
         'admin-script',
-        plugin_dir_url(__DIR__) . 'public/js/app.js',
-        ['jquery', 'bootstrap-min'],
+        plugin_dir_url(__DIR__) . 'public/js/app.min.js',
+        ['jquery'],
         '1.0.0',
         true
     );
+
 }
 
 // Shorcode function
@@ -113,3 +192,8 @@ function res_public_popup( $atts, $content = '' ){
     }
 
 }
+
+
+
+
+
